e34450c1cb60e9a5279eec8c1d3280a2f67e989f
maurmair <maurmair@gmail.com>
2021-02-05 21:47:19 +0100

    first

---
+++ src/main/java/net/maurmair/userapi/controllers/UrlCheckController.java
@@ -0,0 +1,40 @@
+package net.maurmair.userapi.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrlCheckController {
+
+    private final String SITE_IS_UP = "Site is up!";
+    private final String SITE_IS_DOWN = "Site is down!";
+    private final String SITE_IS_INCORRECT = "Url is incorrect!";
+
+    @GetMapping("/check")
+    public String getUrlStatusMessage(@RequestParam final String url) {
+        String returnMessage = "";
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responseCodeCatergory = conn.getResponseCode() / 100;
+            if (responseCodeCatergory != 2) {
+                returnMessage = SITE_IS_DOWN;
+            } else {
+                returnMessage = SITE_IS_UP;
+            }
+        } catch (MalformedURLException e) {
+            returnMessage = SITE_IS_INCORRECT;
+        } catch (IOException e) {
+            returnMessage = SITE_IS_DOWN;
+        }
+        return returnMessage;
+    }
+}
